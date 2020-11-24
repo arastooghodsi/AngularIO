ff7ce292bcbd5d216f9dd4b5c25ecc379183031a
arastooghodsi <arastoo.ghodsi.ai@gmail.com>
2020-11-24 16:05:59 +0330

    correct

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -29,7 +29,7 @@ export class HeroesComponent implements OnInit {
 
   public onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=$(hero.id');
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
