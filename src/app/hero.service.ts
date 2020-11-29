3be28fb35025a46e0b53fa03a95c5c4907e6ebf3
arastooghodsi <arastoo.ghodsi.ai@gmail.com>
2020-11-29 11:32:37 +0330

    add search hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -64,4 +64,25 @@ export class HeroService {
     );
   }
 
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`delete hero id = ${id}`)),
+      catchError(this.handleError<Hero>('deletHero'))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
 }
